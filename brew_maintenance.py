e77bdff843b28fa33a0f9b22d01d62d1b1c835a2
argelalan <campos.alan@protonmail.com>
2022-08-06 12:15:29 -0700

    Added 'brew upgrade' functionality.

--- brew_maintenance.py
+++ brew_maintenance.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
+ #!/usr/bin/env python3
 
 import subprocess
 
-update_result = subprocess.run(['brew', 'update'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+subprocess.run(['brew', 'update'])
+outdated_result = subprocess.run(['brew', 'outdated'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-if 'Already up-to-date.' not in update_result.stdout:
-	outdated_result = subprocess.run(['brew', 'outdated'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-else:
-	outdated_result = None
-
-print(update_result)
+if outdated_result.stdout:
+    subprocess.run(['brew', 'upgrade'])
