d63dcc0ba7c6bab9322e716daefb88b9d68cd4cb
argelalan <campos.alan@protonmail.com>
2022-08-06 12:35:02 -0700

    Added .gitignore file.

--- brew_maintenance.py
+++ brew_maintenance.py
@@ -3,7 +3,7 @@
 import subprocess
 
 subprocess.run(['brew', 'update'])
-outdated_result = subprocess.run(['brew', 'outdated'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
+outdated_result = subprocess.run(['brew', 'outdated'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 if outdated_result.stdout:
     subprocess.run(['brew', 'upgrade'])
