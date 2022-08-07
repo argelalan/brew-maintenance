8d3f4b1d78642f379c76c58dbbc36dc1c715a310
argelalan <campos.alan@protonmail.com>
2022-08-07 12:00:25 -0700

    Added full path to 'brew' command for crontab functionality.

--- brew_maintenance.py
+++ brew_maintenance.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
 import subprocess
+from datetime import datetime as dt
+import time
 
-subprocess.run(['brew', 'update'])
 
-outdated_result = subprocess.run(['brew', 'outdated'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+brew_command = '/usr/local/bin/brew'
+
+subprocess.run([brew_command, 'update'])
+outdated_result = subprocess.run([brew_command, 'outdated'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 if outdated_result.stdout:
-    subprocess.run(['brew', 'upgrade'])
+    subprocess.run([brew_command, 'upgrade'])
+
+current_date = dt.now()
+current_localtime = time.localtime()
+current_time = time.strftime('%H:%M:%S', current_localtime)
+log_message = f'Updated at {current_time}'
+print(log_message)
