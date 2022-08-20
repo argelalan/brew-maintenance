358bc8178a285857abca4b6c741beeab352facaf
argelalan <campos.alan@protonmail.com>
2022-08-20 12:29:50 -0700

    Modified log message.

--- brew_maintenance.py
+++ brew_maintenance.py
@@ -14,7 +14,10 @@ if outdated_result.stdout:
     subprocess.run([brew_command, 'cleanup'])
 
 current_date = dt.now()
+current_date = current_date.strftime('%m/%d/%Y')
 current_localtime = time.localtime()
 current_time = time.strftime('%H:%M:%S', current_localtime)
-log_message = f'Updated at {current_time}'
-print(log_message)
+log_message = f'Updated {current_date} - {current_time}\n'
+
+with open('log.txt', 'a') as log_file:
+    log_file.write(log_message)
